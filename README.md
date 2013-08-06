d418fb686060cd78cb3191f0dfb5dc34ab72399d
T. Brian Jones <tbrianjones@gmail.com>
2013-08-06 01:36:52 +0000

    added the client and moved notes over to readme.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 data_science_toolkit_php_api_client
 ===================================
-
 A simple PHP API Client for the Data Science Toolkit project.
+
+
+API Client Usage
+----------------
+
+### getting started
+- include the client class and instantiate it
+- set the base url using `set_base_url()`
+	- not specifying a url will default to the open api address
+
+### Available Methods
+- `street2coordinates( $address )`
+- `google_style_geocoder( $address )`
+- `coordinates2politics( $lat, $lon )`
+
+### Additional Endpoints
+- see developer docs for additional rest endpoints
+- http://www.datasciencetoolkit.org/developerdocs
+
+
+Running Your Own DST Server
+---------------------------
+
+### Launch an AWS Machine Image of the DST
+- doesn't work from the web interface ( generally won't find the AMI ID )
+- prob want to check documentation for the most recent api id
+- from the CLI: `ec2-run-instances ami-7b9df412 -t m1.small -z us-east-1b --aws-access-key <ACCESS-KEY> -W <SECRET-KEY> -k <KEY-FILE> -v`
---
+++ dst_api_client.php
@@ -0,0 +1,82 @@
+<?php
+
+	//
+	// A PHP API Client for the Data Science Toolkit
+	//
+	//	- http://www.datasciencetoolkit.org
+	//	- github repo: https://github.com/petewarden/dstk
+	//
+	
+	class Dst_api_client
+	{
+		
+		private $base_url;
+		
+		public function set_base_url(
+			$base_url = 'http://www.datasciencetoolkit.org/'
+		) {
+			$this->base_url = $base_url;
+		}
+		
+		public function street2coordinates(
+			$address // url encoded or not, doesn't matter
+		) {
+			return $this->do_request( 'street2coordinates/' . urlencode( $address ) );
+		}
+		
+		public function google_style_geocoder(
+			$address // url encoded or not, doesn't matter
+		) {
+			return $this->do_request( 'maps/api/geocode/json?sensor=false &address=' . urlencode( $address ) );
+		}
+		
+		public function coordinates2politics(
+			$lat,
+			$lon
+		) {
+			return $this->do_request( 'coordinates2politics/' . $lat . ',' . $lon );
+		}
+				
+		// make a curl request to the elasticsearch cluster
+		//
+		private function do_request(
+			$uri,						// the uri of the request ( leading slashes are removed )
+			$display_headers = FALSE	// set to TRUE to output the headers ... generally for development and testing
+		) {
+			
+			// remove leading slash if one exists in the passed uri
+			$uri = ltrim( $uri, '/' );
+			
+			// initialize curl
+			$Curl = curl_init();
+			
+			// set basic curl configuration
+			$options = array(
+				CURLOPT_URL				=> $this->base_url . $uri,	// the url of the request
+				CURLOPT_CUSTOMREQUEST	=> 'GET',					// the http request method ( GET, POST, PUT, DELETE )
+				CURLOPT_RETURNTRANSFER	=> true,					// return web page
+				CURLOPT_CONNECTTIMEOUT	=> 2,						// timeout on connect ( seconds )
+				CURLOPT_TIMEOUT			=> 2						// timeout on response ( seconds )
+			);
+			
+			// make curl request
+			curl_setopt_array( $Curl, $options );
+			$response			= '';
+			$response			= curl_exec( $Curl );
+			$error_number		= curl_errno( $Curl );
+			$error_message		= curl_error( $Curl );
+			$header				= curl_getinfo( $Curl );
+			curl_close( $Curl );
+									
+			// output cURL errors if there were any
+			if( $error_number > 0 ) {
+				throw new Exception( "Data Science Toolkit server could not be reached due to a cURL connection error. cURL Error $error_number: $error_message" );
+			}
+			
+			return $response;
+						
+		}
+		
+	}
+
+?>
