c9dd6894a2c9e007693065126c9b272454d83333
T. Brian Jones <tbrianjones@gmail.com>
2014-01-31 18:41:59 -0800

    Updated client so it removes PO Boxes
    The dst struggled with po boxes.  The client will now extract the po box if there is no street address in the remaining address.  the client will also attempt to extract the street if the dst fails to ... which it does sometimes.

--- dst_api_client.php
+++ dst_api_client.php
@@ -1,10 +1,24 @@
 <?php
 
-	//
-	// A PHP API Client for the Data Science Toolkit
+	// a php api client for the data science toolkit
 	//
 	//	- http://www.datasciencetoolkit.org
 	//	- github repo: https://github.com/petewarden/dstk
+	//
+	//	- launch your own amazon machine image of the data science toolkit:
+	//		- doesn't work from the web interface
+	//		- prob want to check documentation for the most recent api id
+	//		- from the CLI: `ec2-run-instances ami-7b9df412 -t m1.small -z us-east-1b --aws-access-key <ACCESS-KEY> -W <SECRET-KEY> -k <KEY-FILE> -v`
+	//
+	
+	// API Client Usage:
+	//
+	//	- include this class
+	//	- instantiate class
+	//	- set the base url
+	//		- not specifying a url will default to the open api address
+	//	- see developer docs for rest endpoints
+	//		- http://www.datasciencetoolkit.org/developerdocs
 	//
 	
 	class Dst_api_client
@@ -12,7 +26,6 @@
 		
 		private $base_url;
 		
-		// if no url is passed, then the public DST server will be used
 		public function set_base_url(
 			$base_url = 'http://www.datasciencetoolkit.org/'
 		) {
@@ -20,15 +33,37 @@
 		}
 		
 		public function street2coordinates(
-			$address // string containing address
+			$address // url encoded or not, doesn't matter
 		) {
-			return $this->do_request( 'street2coordinates/' . urlencode( $address ) );
+		
+		  // clean up address for DST ( dst struggles with slashes, new lines, and po boxes )
+			$address = str_replace( "\n", ' ', $address );
+			$address = str_replace( "/", '', $address );
+			$dst_address = preg_replace( '/((po|p\.o\.)\sbox\s[0-9]{1,8})/i', ' ', $address ); // remove po boxes
+			$dst_address = urlencode( $dst_address );
+			$response_json = $this->do_request( 'street2coordinates/' . $dst_address );
+			
+			// check for a street ( DST struggles with some streets and with po boxes )
+      $response_array = json_decode( $response_json, TRUE );
+      foreach( $response_array as $data ) {
+        if( is_null( $data['street_address'] ) OR $data['street_address'] == '' ) {
+          $data['street_address'] = $this->extract_street( $address );
+          $array[$address] = $data;
+          $response_json = json_encode( $array );
+        }
+        break(1);
+      }
+      
+      return $response_json;
+  			
 		}
 		
+		// the streets2coordinates endpoint does some error correcting for po boxes and missing streets
+		//  these changes have not been adapted to this endpoint, but should be
 		public function google_style_geocoder(
-			$address // string containing address
+			$address // url encoded or not, doesn't matter
 		) {
-			return $this->do_request( 'maps/api/geocode/json?sensor=false &address=' . urlencode( $address ) );
+			return $this->do_request( 'maps/api/geocode/json?sensor=false &address=' . urlencode( str_replace( "\n", ' ', $address ) ) );
 		}
 		
 		public function coordinates2politics(
@@ -38,13 +73,13 @@
 			return $this->do_request( 'coordinates2politics/' . $lat . ',' . $lon );
 		}
 				
-		// make a curl request to the dst server specified by $this->set_base_url()
+		// make a curl request to the elasticsearch cluster
 		//
 		private function do_request(
 			$uri,						// the uri of the request ( leading slashes are removed )
 			$display_headers = FALSE	// set to TRUE to output the headers ... generally for development and testing
 		) {
-			
+						
 			// remove leading slash if one exists in the passed uri
 			$uri = ltrim( $uri, '/' );
 			
@@ -56,8 +91,8 @@
 				CURLOPT_URL				=> $this->base_url . $uri,	// the url of the request
 				CURLOPT_CUSTOMREQUEST	=> 'GET',					// the http request method ( GET, POST, PUT, DELETE )
 				CURLOPT_RETURNTRANSFER	=> true,					// return web page
-				CURLOPT_CONNECTTIMEOUT	=> 2,						// timeout on connect ( seconds )
-				CURLOPT_TIMEOUT			=> 2						// timeout on response ( seconds )
+				CURLOPT_CONNECTTIMEOUT	=> 5,						// timeout on connect ( seconds )
+				CURLOPT_TIMEOUT			=> 10						// timeout on response ( seconds )
 			);
 			
 			// make curl request
@@ -78,6 +113,74 @@
 						
 		}
 		
-	}
+		// this will extract the street from a raw address
+		//
+		//  - this returns the street component ( eg. 4585 Del Monte Ave., or PO Box 792 )
+		//
+		//  - this should only be used when the DST does not return a street
+		//    - the dst fails on some streets
+		//    - it also fails on po boxes
+		//    - it also seems to fail on raw addresses that have both a po box and a street
+		//  
+		//  - this solution is not 100%, but it's very good
+		//
+		private function extract_street( $raw_address ) {
+  		
+  		// regexes for po box extraction
+      $po_box_regex = '/((po|p\.o\.)\sbox\s[0-9]{1,8})/i';
+  		
+  		// street types to look for and clean up ( this very is good, but not exhaustive )
+      $street_types = array();
+      $street_types[] = array( "st|street", "St" );
+      $street_types[] = array( "dr|drive", "Dr" );
+  		$street_types[] = array( "ct|court", "Ct" );
+  		$street_types[] = array( "rd|road", "Rd" );
+      $street_types[] = array( "ln|lane", "Ln" );
+      $street_types[] = array( "ave|avenue", "Ave" );
+      $street_types[] = array( "blvd|boulevard", "Blvd" );
+      $street_types[] = array( "cir|circle", "Cir" );
+      $street_types[] = array( "dr|place", "Pl" );
+      $street_types[] = array( "way", "Way" );
+      $street_types[] = array( "pkwy|parkway", "Pkwy" );
+      $street_types[] = array( "ter|terrace", "Ter" );
+
+      // build street extraction regex
+      $street_regex = '/(\d{1,5}\s[^\d].{5,20}\s(';
+      foreach( $street_types as $street_type )
+        $street_regex .= $street_type[0].'|';
+      $street_regex = trim( $street_regex, '|' );
+      $street_regex .= ')(\.|\s|\,))/i';
+
+      // look for a street first ( we'd rather have a street address than a po box )
+      if( preg_match( $street_regex, $raw_address, $match ) ) {
+        // cleanse to match DST format
+        $st = $match[0];
+        foreach( $street_types as $street_type ) {
+          $find = '/((\s)('.$street_type[0].')(\.|\,|\s))/i';
+          $replace = '$2'.$street_type[1].'$4';
+          if( preg_replace( $find, $replace, $st ) != $st ) {
+            $st = preg_replace( $find, $replace, $st );
+            break;
+          }
+        }        
+        return trim( $st );
+      }
+      
+      // then look for a po box
+      else if( preg_match( $po_box_regex, $raw_address, $match ) ) {
+        // cleanse to PO BOX 123 format
+        $po = $match[0];
+        $find = '/((po|p\.o\.)\sbox\s)/i';
+        $replace = 'PO BOX ';
+        return trim( preg_replace( $find, $replace, $po ) );
+      }
+      
+      // nothing was found, return false
+      else
+        return '';
+        
+		}
+		
+	} // end class
 
 ?>
